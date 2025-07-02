e2f314dab444aa9d1e3b02920f08b36d08ddeb32
Gautamkrishna2002 <krishnagautamchinta004@gmail.com>
2025-07-02 18:34:22 +0530

    commit

---
+++ Collection/src/list/arrarylist/Demo.java
@@ -0,0 +1,31 @@
+package list.arrarylist;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		List al = new ArrayList();
+		
+		al.add(10);
+		al.add(12.2);
+		al.add('s');
+		al.add("Hello World");
+		al.add(true);
+		al.add(null);
+		al.add('s');
+		
+		System.out.println(al);
+		System.out.println(al.size());
+		al.remove("Hello World");
+		System.out.println(al);
+		System.out.println(al.size());
+		al.add("Hello World");
+		System.out.println(al);
+		System.out.println(al.size());
+		System.out.println(al.get(4));
+
+	}
+
+}
