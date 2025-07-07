50d88d6b7f2ce973ea6c272e8248ede68b39b66c
Gautamkrishna2002 <krishnagautamchinta004@gmail.com>
2025-07-07 16:31:04 +0530

    commit

---
+++ Collection/src/list/arraylist/question/Problems.java
@@ -0,0 +1,23 @@
+package list.arraylist.question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problems {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List al = new ArrayList();
+		al.add("Raj");
+		al.add(23.2);
+		al.add(true);
+		al.add(10);
+		al.add(null);
+		al.add('e');
+		al.add("luvkush");
+		System.out.println(al);
+
+	}
+
+}
